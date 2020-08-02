d439371a2241d6a639385f5cf16c402b80151023
LovelySweety418 <69114040+LovelySweety418@users.noreply.github.com>
2020-08-02 23:05:32 +0800

    Add files via upload

---
+++ Discord Bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+
+
+client.once('ready', () => {
+    console.log('Im online!');
+});
+
+
+
+client.login('NzM5NDcwNzY2MTAxMzY0ODA3.Xya70Q.IViSb84qbBskg2eb_4_rfLc7H2E');
